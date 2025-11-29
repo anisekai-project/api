3e4016c8046818a25b77e54d5c20b7c8dce3adcd
alexpado <contact@alexpado.fr>
2025-11-29 23:30:41 +0100

    fix(app): Enable JPA Auditing
    This allows @CreatedDate and @LastModifiedDate annotation to work properly

--- src/main/java/fr/anisekai/AnisekaiApplication.java
+++ src/main/java/fr/anisekai/AnisekaiApplication.java
@@ -5,6 +5,7 @@ import fr.anisekai.discord.DiscordService;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableJpaRepositories(repositoryBaseClass = AnisekaiRepositoryImpl.class)
+@EnableJpaAuditing
 public class AnisekaiApplication {
 
     @SuppressWarnings({"StaticNonFinalField", "CanBeFinal"})
